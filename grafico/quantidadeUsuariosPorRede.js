7a23f5182130bbeda272dbe6b80840e44f7ae55e
Eloá <111518085+eloaandreatta@users.noreply.github.com>
2024-10-08 12:22:43 -0300

    Create quantidadeUsuariosPorRede.js

---
+++ grafico/quantidadeUsuariosPorRede.js
@@ -0,0 +1,23 @@
+async function quantidadeusuarios(){
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json&#39'
+    const res = await fetch (url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados);
+
+    const data = [
+        {
+            x: nomeDasRedes, 
+            y: quantidadeUsuarios,
+            type: 'bar'
+    
+        }
+    ]
+    
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Ploty.newPlot(grafico,data)
+
+}
+
+quantidadeUsuariosPorRede();
