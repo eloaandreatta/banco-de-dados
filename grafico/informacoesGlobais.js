aac3474c8ac1cafec19dcfce403afec2c9646995
Eloá <111518085+eloaandreatta@users.noreply.github.com>
2024-10-08 12:31:31 -0300

    Update informacoesGlobais.js

--- grafico/informacoesGlobais.js
+++ grafico/informacoesGlobais.js
@@ -5,6 +5,8 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function visualizarInformacoesGlobais() {
     const res= await fetch(url)
     const dados = await res.json
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_de_pessoas_mundo / 1e9)
     console.log(dados);
     const paragrafo = document.createElement('p');
     paragrafo.classList.add('graficos-container__texto');
